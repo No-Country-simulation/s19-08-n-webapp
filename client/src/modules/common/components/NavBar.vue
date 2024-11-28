e0ed8e8f4a7bc54a6c024c3ca3ac147bb5fe7131
jumagu <jmgg.gutierrez@gmail.com>
2024-11-28 14:19:30 -0500

    refactor: improve responsiveness and use navmenu component

--- client/src/modules/common/components/NavBar.vue
+++ client/src/modules/common/components/NavBar.vue
@@ -1,24 +1,59 @@
 <script setup lang="ts">
+import NavMenu from './NavMenu.vue';
+import MenuIcon from '../icons/MenuIcon.vue';
+import GearIcon from '../icons/GearIcon.vue';
+import BellIcon from '../icons/BellIcon.vue';
 import LogOutIcon from '../icons/LogOutIcon.vue';
 import SearchIcon from '../icons/SearchIcon.vue';
+import ProfileIcon from '../icons/ProfileIcon.vue';
 import ThemeController from './ThemeController.vue';
+
+interface Props {
+  search?: boolean;
+  navLinks?: boolean;
+}
+
+withDefaults(defineProps<Props>(), { search: false, navLinks: false });
 </script>
 
 <template>
-  <header class="navbar bg-base-100 sticky top-0 z-[1] shadow-lg">
-    <div class="hidden sm:block flex-1">
-      <a class="btn btn-ghost text-xl">FreeConnect</a>
+  <header class="navbar min-h-12 bg-base-100 sticky top-0 z-[1] shadow-lg">
+    <div class="lg:navbar-start">
+      <div v-if="navLinks" class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-sm lg:hidden">
+          <menu-icon class="size-5" />
+        </div>
+        <nav-menu
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow-lg"
+        />
+      </div>
+      <router-link to="/home" class="btn btn-ghost text-xl gap-0 hidden lg:flex">
+        <span class="text-primary">Free</span>
+        <span class="text-secondary">Connect</span>
+      </router-link>
     </div>
-    <div class="flex-1 sm:flex-none gap-2">
-      <div class="form-control w-full">
-        <label class="input input-primary input-bordered flex items-center gap-2 bg-base-200">
-          <input type="search" class="grow" :placeholder="$t('common.navbar.search')" />
-          <search-icon class="h-4 w-4 opacity-70" />
+
+    <div v-if="navLinks" class="flex-shrink-0 hidden lg:flex">
+      <nav-menu class="menu menu-horizontal py-0" />
+    </div>
+
+    <div class="justify-end w-full lg:w-1/2 gap-2">
+      <div v-if="search" class="w-full lg:w-auto">
+        <label
+          class="input input-sm md:input-md input-primary input-bordered flex items-center gap-2 bg-base-200"
+        >
+          <input type="search" class="w-full" :placeholder="$t('common.navbar.search')" />
+          <search-icon class="size-4 opacity-70" />
         </label>
       </div>
-      <div class="dropdown dropdown-end">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-          <div class="w-10 rounded-full">
+      <div>
+        <button class="btn btn-sm md:btn-md btn-circle">
+          <bell-icon class="size-4 md:size-6" />
+        </button>
+      </div>
+      <div class="dropdown dropdown-end flex md:inline-block">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle btn-sm md:btn-md avatar">
+          <div class="size-10 rounded-full">
             <img
               alt="Tailwind CSS Navbar component"
               src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
@@ -27,14 +62,8 @@ import ThemeController from './ThemeController.vue';
         </div>
         <ul
           tabindex="0"
-          class="menu menu-md dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow-lg"
+          class="menu menu-md dropdown-content bg-base-100 rounded-box z-[1] mt-11 md:mt-3 w-52 p-2 shadow-lg"
         >
-          <li>
-            <a>{{ $t('common.navbar.profile') }}</a>
-          </li>
-          <li>
-            <a>{{ $t('common.navbar.settings') }}</a>
-          </li>
           <li>
             <div class="justify-between !cursor-auto">
               {{ $t('common.navbar.appearance') }}
@@ -42,9 +71,21 @@ import ThemeController from './ThemeController.vue';
             </div>
           </li>
           <li>
+            <router-link to="/profile/juan" class="justify-between">
+              {{ $t('common.navbar.profile') }}
+              <profile-icon class="size-4" aria-hidden="true" />
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/settings" class="justify-between">
+              {{ $t('common.navbar.settings') }}
+              <gear-icon class="size-4" aria-hidden="true" />
+            </router-link>
+          </li>
+          <li>
             <button type="button" class="justify-between">
               <span>{{ $t('common.navbar.logout') }}</span>
-              <log-out-icon class="w-4 h-4" />
+              <log-out-icon class="size-4" aria-hidden="true" />
             </button>
           </li>
         </ul>
