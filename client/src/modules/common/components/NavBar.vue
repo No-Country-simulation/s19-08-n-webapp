5cc9f43c15fbb8861f69292c75a8a8041fdc60a4
jumagu <jmgg.gutierrez@gmail.com>
2024-12-12 01:01:02 -0500

    refactor: protect sections for only authenticated users

--- client/src/modules/common/components/NavBar.vue
+++ client/src/modules/common/components/NavBar.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+
 import {
   BellIcon,
+  LogInIcon,
   LogOutIcon,
   SearchIcon,
   SettingsIcon,
@@ -10,6 +13,8 @@ import {
 import NavMenu from './NavMenu.vue';
 import MenuIcon from '../icons/MenuIcon.vue';
 import ThemeController from './ThemeController.vue';
+import FreeConnectIcon from '../icons/FreeConnectIcon.vue';
+import { useAuthStore } from '@/modules/auth/stores/auth.store';
 
 interface Props {
   search?: boolean;
@@ -17,26 +22,37 @@ interface Props {
 }
 
 withDefaults(defineProps<Props>(), { search: false, navLinks: false });
+
+const router = useRouter();
+const authStore = useAuthStore();
+
+const logoutHandler = () => {
+  authStore.startLogout();
+  router.replace({ name: 'home' });
+};
 </script>
 
 <template>
   <header class="navbar min-h-12 bg-base-100 sticky top-0 z-[1] shadow-lg">
     <div class="lg:navbar-start">
-      <div v-if="navLinks" class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-sm lg:hidden">
+      <div v-if="navLinks && authStore.isAuthenticated" class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-circle btn-sm lg:hidden mr-2">
           <menu-icon class="size-5" />
         </div>
         <nav-menu
           class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow-lg"
         />
       </div>
-      <router-link to="/home" class="btn btn-ghost text-xl gap-0 hidden lg:flex">
-        <span class="text-primary">Free</span>
-        <span class="text-secondary">Connect</span>
+      <router-link to="/home" class="btn btn-ghost gap-1 hidden lg:inline-flex">
+        <free-connect-icon class="size-7" />
+        <div class="text-xl">
+          <span class="text-primary">Free</span>
+          <span class="text-secondary">Connect</span>
+        </div>
       </router-link>
     </div>
 
-    <div v-if="navLinks" class="flex-shrink-0 hidden lg:flex">
+    <div v-if="navLinks && authStore.isAuthenticated" class="flex-shrink-0 hidden lg:flex">
       <nav-menu class="menu menu-horizontal py-0" />
     </div>
 
@@ -49,18 +65,22 @@ withDefaults(defineProps<Props>(), { search: false, navLinks: false });
           <search-icon :size="16" class="opacity-70" />
         </label>
       </div>
-      <div>
-        <button class="btn btn-sm md:btn-md btn-circle">
-          <bell-icon class="size-4 md:size-6" />
+      <div v-if="authStore.isUnauthenticated" class="flex-shrink-0">
+        <router-link to="/auth/login" class="btn btn-primary btn-outline btn-sm md:btn-md">
+          {{ $t('common.navbar.login') }}
+          <log-in-icon :size="16" aria-hidden="true" />
+        </router-link>
+      </div>
+      <div v-if="authStore.isAuthenticated">
+        <button type="button" class="btn btn-sm md:btn-md btn-circle">
+          <span class="sr-only">{{ $t('common.navbar.notifications') }}</span>
+          <bell-icon class="size-4 md:size-6" aria-label="true" />
         </button>
       </div>
-      <div class="dropdown dropdown-end flex md:inline-block">
+      <div v-if="authStore.isAuthenticated" class="dropdown dropdown-end flex md:inline-block">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle btn-sm md:btn-md avatar">
           <div class="rounded-full">
-            <img
-              alt="Tailwind CSS Navbar component"
-              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-            />
+            <img :src="authStore.profileImg" :alt="authStore.user?.userName" />
           </div>
         </div>
         <ul
@@ -86,7 +106,7 @@ withDefaults(defineProps<Props>(), { search: false, navLinks: false });
             </router-link>
           </li>
           <li>
-            <button type="button" class="justify-between">
+            <button type="button" class="justify-between" @click="logoutHandler">
               <span>{{ $t('common.navbar.logout') }}</span>
               <log-out-icon :size="16" aria-hidden="true" />
             </button>
